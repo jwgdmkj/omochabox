92403f038fc4f01d9b55cb0bbdfa8315129f2378
jwgdmkj <63781293+jwgdmkj@users.noreply.github.com>
2022-08-20 23:33:27 +0900

    Update usepy360convert.py

--- usepy360convert.py
+++ usepy360convert.py
@@ -62,3 +62,76 @@ for (root, directories, files) in os.walk(rootpath):
                 c_img2.save(makedir + '/' + original_filename + 'L' + '.jpg')
                 c_img3.save(makedir + '/' + original_filename + 'R' + '.jpg')
                 c_img4.save(makedir + '/' + original_filename + 'B' + '.jpg')
+                
+                
+               
+#2중 for문 버전
+
+import numpy as np
+import py360convert
+from PIL import Image
+import os
+
+
+rootpath = '//mldisk.sogang.ac.kr/nfs_shared_/STR_Data/RoadView/muhan_roadview/1796/sphere'
+rootpath2 = '//mldisk.sogang.ac.kr/nfs_shared_/STR_Data/RoadView/muhan_roadview/2098/sphere'
+rootpath3 = '//mldisk.sogang.ac.kr/nfs_shared_/STR_Data/RoadView/muhan_roadview/2101/sphere'
+rootpath = '/nfs_shared/STR_Data/RoadView/muhan_roadview/2098/sphere'
+rootpath = '/nfs_shared/STR_Data/RoadView/muhan_roadview/1796/sphere'
+d_path_direc = []
+
+flag = True
+# os.walk() : 인자로 전달된 경로의 폴더를 재귀적으로 탐색, 모든 파일리스트를 출력
+for (root, directories, files) in os.walk(rootpath):
+
+    if (len(directories)) > 0:
+        for dir_name in directories:
+            if dir_name.endswith('_E'):
+                # print("dir: " + root + '/' + dir_name)
+                d_path_direc.append(root + '/' + dir_name)
+
+# d_path_direc.pop(0)
+d_path_direc.reverse()
+# print(d_path_direc)
+
+for path in d_path_direc:
+    files = os.listdir(path)
+    makedir = '/nfs_shared/STR_Data/RoadView/muhan_roadview_transformed/1796' + '/' + path[57:-3]
+    try:
+        if not os.path.exists(makedir):
+            os.makedirs(makedir)
+    except OSError:
+        pass
+
+    for file in files:
+        # print(path + '/' + file)
+        file_path = path + '/' + file
+        original_filename = file[:-4]
+        if original_filename.startswith('Thu'):
+            print(file_path + '/' + original_filename)
+            continue
+
+        # print(original_filename)
+        # print(makedir + '/' + original_filename + 'F' + '.jpg')
+
+        e_img = np.array(Image.open(file_path))
+        cubemap = py360convert.e2p(e_img, fov_deg = (90, 90), u_deg = -0,
+                                                v_deg = 0, out_hw = (2000, 2000), in_rot_deg = 0)
+        c_img = Image.fromarray(cubemap)
+        cubemap2 = py360convert.e2p(e_img, fov_deg=(90, 90), u_deg=-90,
+                                               v_deg=0, out_hw=(2000, 2000), in_rot_deg=0)
+        c_img2 = Image.fromarray(cubemap2)
+        cubemap3 = py360convert.e2p(e_img, fov_deg=(90, 90), u_deg=90,
+                                               v_deg=0, out_hw=(2000, 2000), in_rot_deg=0)
+        c_img3 = Image.fromarray(cubemap3)
+        cubemap4 = py360convert.e2p(e_img, fov_deg=(90, 90), u_deg=180,
+                                               v_deg=0, out_hw=(2000, 2000), in_rot_deg=0)
+        c_img4 = Image.fromarray(cubemap4)
+
+        c_img.save(makedir + '/' + original_filename + 'F' + '.jpg')
+        c_img2.save(makedir + '/' + original_filename + 'L' + '.jpg')
+        c_img3.save(makedir + '/' + original_filename + 'R' + '.jpg')
+        c_img4.save(makedir + '/' + original_filename + 'B' + '.jpg')
+
+
+
